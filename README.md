ad2790c0b374b30def7c247c8fa312114b974cae
Jorge <jorge97fernandez@gmail.com>
2020-12-16 11:24:37 +0100

    AgenteInit V1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ## Pasos a realizar para la creación automatica de agente init
 * Modificar el valor de Argumentos por: -gui <-local-host 127.0.0.1>(solo Linux) -agents init:agentes.AgenteInit
+
+* Incluir argumento numero de iteraciones: -gui <-local-host 127.0.0.1>(solo Linux) -agents init:agentes.AgenteInit(2)
--- src/agentes/AgenteInit.java
+++ src/agentes/AgenteInit.java
@@ -6,23 +6,44 @@ import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 
+import static java.lang.System.exit;
+
 public class AgenteInit extends Agent {
     private static int NUM_EXEC= 5;
+    private static final String  modelos[]={"J48","NaiveBayes","MultilayerPerceptron"};
+    private static final int  porcentajes[]={30,50,80};
     public class Iniciar_Agentes extends OneShotBehaviour{
         public void action(){
             ContainerController cc = getContainerController();
             AgentController ac = null;
-            int iteraciones = AgenteInit.NUM_EXEC;
+            Object[] arguments=getArguments();
+            if (arguments!= null && arguments.length>=1 && ! arguments[0].toString().equals("")){
+                try {
+                    AgenteInit.NUM_EXEC = Integer.parseInt(arguments[0].toString());
+                }catch (Exception e){
+                    System.err.println("ERROR: El argumento introducido no es un numero entero");
+                    exit(1);
+                }
+            }
             try {
-                ac = cc.createNewAgent("pantalla", "agentes.AgenteResultado", null);
-                ac.start();
-                ac = cc.createNewAgent("dm", "agentes.AgenteDM", null);
-                ac.start();
+                for (String cad:AgenteInit.modelos){
+                    ac = cc.createNewAgent("resultado-" + cad, "agentes.AgenteResultado", null);
+                    ac.start();
+                }
+                for (int i=0; i< AgenteInit.NUM_EXEC;i++){
+                    for (String cad:AgenteInit.modelos){
+                        for (int porcentaje_modelo:AgenteInit.porcentajes){
+                            ac = cc.createNewAgent("dm-" + cad + "-" + porcentaje_modelo+ "-" + (i+1), "agentes.AgenteDM", null);
+                            ac.start();
+                        }
+                    }
+                }
                 ac = cc.createNewAgent("archivo", "agentes.AgenteArchivo", null);
                 ac.start();
             } catch (StaleProxyException e) {
                 e.printStackTrace();
             }
+            doDelete();
         }
     }
     /*Asignacion de comportamientos*/
