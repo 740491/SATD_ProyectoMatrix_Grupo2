ad8bf79c3d4ce6c0e8231fa4dfa0925f99a14bf7
Jorge <jorge97fernandez@gmail.com>
2021-01-12 19:10:27 +0100

    Init V1.5

--- src/agentes/AgenteInit.java
+++ src/agentes/AgenteInit.java
@@ -6,12 +6,17 @@ import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static java.lang.System.exit;
 
 public class AgenteInit extends Agent {
     private static int NUM_EXEC= 1;
+    private static String[] RESISTENCIA_INIT = {"Neo","Morfeo","Triniti"};
+    private static String[] SISTEMA_INIT = {"Smith","Torrente","Terminator"};
+    private ArrayList<String> agentes_JP = new ArrayList<>();
     public class Iniciar_Agentes extends OneShotBehaviour{
         public void action(){
             ContainerController cc = getContainerController();
@@ -26,18 +31,27 @@ public class AgenteInit extends Agent {
                 }
             }
             try {
-                    ac = cc.createNewAgent("joepublic", "agentes.AgenteJoePublic", null);
+                for(int i=0; i<NUM_EXEC;i++) {
+                    ac = cc.createNewAgent("joepublic-"+ i, "agentes.AgenteJoePublic", null);
                     ac.start();
+                    agentes_JP.add("joepublic-"+ i);
+                }
                     ac = cc.createNewAgent("oraculo", "agentes.Oraculo", null);
                     ac.start();
-                    ac = cc.createNewAgent("resistencia", "agentes.AgenteResistencia", null);
-                    ac.start();
+                    for(String agente:RESISTENCIA_INIT) {
+                        ac = cc.createNewAgent(agente, "agentes.AgenteResistencia", null);
+                        ac.start();
+                    }
                     ac = cc.createNewAgent("resultado", "agentes.AgenteResultado", null);
                     ac.start();
                     Object argumentos[]= new String[]{"arquitecto"};
-                    ac = cc.createNewAgent("sistema", "agentes.AgenteSistema", argumentos);
-                    ac.start();
-                    ac = cc.createNewAgent("arquitecto", "agentes.AgenteArquitecto", null);
+                    for(String agente:SISTEMA_INIT) {
+                        ac = cc.createNewAgent(agente, "agentes.AgenteSistema", argumentos);
+                        ac.start();
+                    }
+                    argumentos= new String[]{};
+                    argumentos=agentes_JP.toArray(argumentos);
+                    ac = cc.createNewAgent("arquitecto", "agentes.AgenteArquitecto", argumentos);
                     ac.start();
             } catch (StaleProxyException e) {
                 e.printStackTrace();
