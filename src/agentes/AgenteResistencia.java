d3fe490b681ae8d241391834119119f2ae44de81
Tisho <Tisho@DESKTOP-MR0NDS6>
2021-01-16 22:10:52 +0100

    Arreglar errores Resistencia/sistema

--- src/agentes/AgenteResistencia.java
+++ src/agentes/AgenteResistencia.java
@@ -47,7 +47,7 @@ public class AgenteResistencia extends Agent {
     public class Sistema_behaviour extends CyclicBehaviour {
 
         //Avisa al arquitecto con bucles para reintentar y todo
-        public void avisar_arquitecto(String contenido){ //TODO:
+        public void avisar_arquitecto(String contenido){
             boolean recopilador_agree = false;
             boolean recopilador_inform = false;
             
@@ -88,7 +88,7 @@ public class AgenteResistencia extends Agent {
         public tipoResultado rotar(String res1){
             if(res1.equals(tipoResultado.EXITO.name())){
                 return tipoResultado.FRACASO;
-            }else if(res1.equals(tipoResultado.EXITO.name())){
+            }else if(res1.equals(tipoResultado.FRACASO.name())){
                 return tipoResultado.EXITO;
             }else{
                 return tipoResultado.EMPATE;
@@ -117,7 +117,7 @@ public class AgenteResistencia extends Agent {
                     if(dec == tipoDecision.COMBATE){
                         ACLMessage query = new ACLMessage(ACLMessage.QUERY_REF);
                         query.addReceiver(arquitecto);
-                        query.setContent(tipoMensaje.ATACAR.name() + "," + tipoAgente.RESISTENCIA.name() + "," + Integer.toString(bonus));//TODO: AgenteSistema
+                        query.setContent(tipoMensaje.ATACAR.name() + "," + tipoAgente.RESISTENCIA.name() + "," + Integer.toString(bonus));
                         this.myAgent.send(query);
 
                         timeouts = MAX_TIMEOUTS;
@@ -141,7 +141,7 @@ public class AgenteResistencia extends Agent {
             }
             else if(ACLMessage.AGREE == mensaje.getPerformative() ){
                 ocupado = true;
-                timeouts = MAX_TIMEOUTS * 2;
+                timeouts = MAX_TIMEOUTS;
             }
             //Nos llega un agente o información
             else if(ACLMessage.INFORM_REF == mensaje.getPerformative() ){
@@ -149,7 +149,7 @@ public class AgenteResistencia extends Agent {
                 timeouts = MAX_TIMEOUTS;
                 String content[] = mensaje.getContent().split(",");
                 
-                if(content[0].equals(tipoAgente.SISTEMA.name())){ //TODO: SISTEMA
+                if(content[0].equals(tipoAgente.SISTEMA.name())){
                         ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
                         request.addReceiver(new AID(content[1], AID.ISLOCALNAME));
                         request.setContent(tipoAccion.COMBATE + "," + tipoAgente.RESISTENCIA + "," + String.valueOf(bonus));
@@ -186,9 +186,9 @@ public class AgenteResistencia extends Agent {
                 
                 if(content[0].equals(tipoAccion.COMBATE.name())){
                     ocupado = false;
-                    if(content[1] == tipoResultado.EXITO.name()){
+                    if(content[1].equals(tipoResultado.EXITO.name())){
                         if(bonus < max_bonus) bonus++;
-                    }else if(content[1]==tipoResultado.EMPATE.name()){
+                    }else if(content[1].equals(tipoResultado.EMPATE.name())){
                         bonus--;
                     }else{
                         System.out.println("Descanse en paz, agenge resistencia " + this.myAgent.getLocalName());
@@ -224,7 +224,7 @@ public class AgenteResistencia extends Agent {
                         this.myAgent.send(inform);
                         
                         //Informar a arquitecto
-                        if(res==tipoResultado.FRACASO.name()){ //Morir
+                        if(res.equals(tipoResultado.FRACASO.name())){ //Morir
                             System.out.println("Descanse en paz, agenge resistencia " + this.myAgent.getLocalName());
                             myAgent.doDelete();
                         }
@@ -238,7 +238,7 @@ public class AgenteResistencia extends Agent {
                         if(bonus>max_bonus) bonus=max_bonus;
                         
                         ACLMessage inform = new ACLMessage(ACLMessage.INFORM);
-                        agree.setContent(tipoAccion.CONOCERORACULO.name());
+                        inform.setContent(tipoAccion.CONOCERORACULO.name());
                         inform.addReceiver(mensaje.getSender());
                         
                         this.myAgent.send(inform);
