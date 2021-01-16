c89966b97b2d780c3720a82dd4d0973d5c172417
alexitoguiz <alessruizsss@gmail.com>
2021-01-16 17:10:42 +0100

    BasicJoePublic

--- src/agentes/AgenteJoePublic.java
+++ src/agentes/AgenteJoePublic.java
@@ -6,6 +6,7 @@
 package agentes;
 
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.FIPANames;
 import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
@@ -15,6 +16,41 @@ import jade.lang.acl.MessageTemplate;
  * @author nacho
  */
 public class AgenteJoePublic extends Agent {
+    
+    public class JoePublic_behaviour extends CyclicBehaviour {
+        /* Comportamiento Agente JoePublic
+        * 
+        * Blocking receive hasta recibir REQUEST desde AgenteSistema o AgenteResistencia
+        * 
+        * RECLUTAR,tipoAgente, bonus (prob reclutamiento), RECLUTAR (exito o fracaso)
+        * Gestionar si el reclutamiento es para uno u otro agente
+        * Probabilidad te dejas reclutar o no
+        * 
+        * Mandar agree de ok reclutado
+        *
+        * REQUEST a AgenteArquitecto
+        * */
+        @Override
+        public void action(){
+        
+            ACLMessage mensaje = myAgent.blockingReceive();
+        
+            if(ACLMessage.REQUEST == mensaje.getPerformative() ){
+                String content[] = mensaje.getContent().split(",");
+                if(content[0] == MensajesComunes.tipoAgente.SISTEMA.name()){
+                    /*Se recibe el REQUEST del Agente Sistema*/
+                    ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+                    
+                    this.myAgent.send(request);
+                }else if(content[0] == MensajesComunes.tipoAgente.RESISTENCIA.name()){
+                    /*Se recibe el REQUEST del Agente Resistencia*/
+                    ACLMessage request = new ACLMessage(ACLMessage.REQUEST);
+                
+                    this.myAgent.send(request);
+                }
+            }
+    }
+    }
     protected void setup()
     {
         MessageTemplate protocolo = MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);// Crear un MessageTemplate de tipo FIPA_REQUEST;
@@ -27,4 +63,5 @@ public class AgenteJoePublic extends Agent {
     protected void takeDown(){
         System.out.println("El agente " + getLocalName() + " muere");
     }    
+    
 }
